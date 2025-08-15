957b1e78df6d9963a226c063b2ce278f83964015
mcrj344 <3892036249@qq.com>
2025-08-15 15:47:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: '墨小晨',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '墨小晨的自留地',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
